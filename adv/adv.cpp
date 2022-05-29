0f0408511fae77de2e3b7dcab0869fec45cb0e69
Shunyang Li <shunyang.li@unsw.edu.au>
2022-05-29 19:14:55 +1000

    go home and work

--- adv/adv.cpp
+++ adv/adv.cpp
@@ -5,6 +5,48 @@
 #include "adv.h"
 #include "../config/config.h"
 
+
+/**
+ * compute the core time neighbor,
+ * the CT neighbors of u is a hash table where each key is a vertex v
+ * (u,v) in G[ts,te], each value of v is the number of edges (u,v,t)
+ * with ts <= t <= te
+ * @param ts strating time
+ * @param cn neighbor
+ * @param num end u id
+ * @param ctn ctn
+ */
+auto compute_ctn(const vid_t& ts,  const int& te,
+                 const int& alpha, const int& beta, BiGraph& g) -> void {
+    // TODO te should be the end time
+    for (auto index = g.edges_idx[ts]; index < g.edges_idx[te]; index++) {
+        auto u = g.edges[index].first;
+        auto v = g.edges[index].second;
+
+        // firstly check u is in abcore, and v is in abcore
+        if (g.left_index[u].size() - 1 < alpha) continue;
+        if (g.left_index[u][alpha] < beta) continue;
+        if (g.right_index[v].size() - 1 < beta) continue;
+        if (g.right_index[v][beta] < alpha) continue;
+
+        // then search the neighbor of u and v
+        
+    }
+
+//    for (auto u = 0; u < u_max; u ++) {
+//        ctn[u].clear();
+//
+//        for (auto index = int(nu[u].size() - 1); index >= 0; index --) {
+//            auto v = nu[u][index].first;
+//            auto t = nu[u][index].second;
+//
+//            // because time from largest to smallest
+//            if (t < ts) break;
+//            ctn[u][v] += 1;
+//        }
+//    }
+}
+
 /**
  * implement the front to end algorithms
  * based on the core values
@@ -15,11 +57,22 @@ auto tabcore_adv(BiGraph& g) -> void {
 
     coreIndexKCore(g);
 
-    for (auto alpha = 1; alpha < g.a_to_b.size(); alpha ++) {
+    // firstly into the core number from 1 - tmax
+    // TODO
+
+    for (auto alpha = 2; alpha < g.a_to_b.size(); alpha ++) {
         if (g.a_to_b[alpha] == 0) continue;
         for (auto beta = 1; beta < g.a_to_b[alpha]; beta ++) {
             // then ts from 1 to tmax
+            for (auto ts = 0; ts < g.tmax; ++ ts) {
+                // TODO then delelte edges
+                /*
+                 * the delete edge is similary with baseline
+                 * need to implement CTN(u)k, need to consider how to implement
+                 * and how to manage the core numbers
+                 */
 
+            }
         }
     }
 
--- baseline/baseline.h
+++ baseline/baseline.h
@@ -8,8 +8,9 @@
 #include "../bigraph/bigraph.h"
 
 auto index_baseline(BiGraph& g) -> void ;
-auto baseline_query(const int& alpha, const int& beta, const int& ts, const int& te, BiGraph& g, vector<bool>& node_u,
-                    vector<bool>& node_v) -> void ;
+auto baseline_query(const int& alpha, const int& beta, const int& ts, const int& te, BiGraph& g, vector<bool>& node_u,vector<bool>& node_v) -> void ;
 
+auto back_del_edges(BiGraph& g, BiGraph& tg, const int& ts, const int& tmax) -> void;
+auto compute_core_neighbor(const vid_t& ts, vector<unordered_map<int, int>>& cn, const num_t& num, vector<vector<pair<vid_t,vid_t>>>& nu) -> void;
 
 #endif //TEMPORAL_ABCORE_BASELINE_H
